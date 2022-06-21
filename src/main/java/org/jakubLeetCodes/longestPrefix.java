c0af38f40dfae62c34f6866959e42ed4addb8e42
Jacob <lewandowskiJakub9@gmail.com>
2022-06-20 22:51:31 -0400

    ValidParentheses

--- src/main/java/org/jakubLeetCodes/longestPrefix.java
+++ src/main/java/org/jakubLeetCodes/longestPrefix.java
@@ -32,16 +32,20 @@ public class longestPrefix {
     }
 
     public static String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0) {
-            return "";
-        }
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++)
-            while (strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
-            }
-        System.out.println(prefix);
-        return prefix;
+       if(strs.length == 0) {
+           return "";
+       }
+
+       String prefix = strs[0];
+       for (int i = 1; i < strs.length; i++) {
+           while (strs[i].indexOf(prefix) != 0) {
+               prefix = prefix.substring(0, prefix.length() -1);
+               System.out.println(prefix);
+               if (prefix.isEmpty()) {
+                   return "";
+               }
+           }
+       }
+       return prefix;
     }
 }
