f068efd62b04ec9063945abfab7ee02d7823a3fd
Jacob <lewandowskiJakub9@gmail.com>
2022-09-08 22:32:59 -0400

    oneEditAway

--- src/main/java/org/jakubLeetCodes/OneEditAway.java
+++ src/main/java/org/jakubLeetCodes/OneEditAway.java
@@ -7,8 +7,12 @@ public class OneEditAway {
     }
 
     public static boolean oneEditAway(String first, String second) {
-        if(first.length() == second.length()) {
+        if (first.length() == second.length()) {
             return oneEditReplace(first, second);
+        } else if (first.length() + 1 == second.length()) {
+            return oneEditInsert(first, second);
+        } else if (first.length() - 1 == second.length()) {
+            return oneEditInsert(second, first);
         }
         return false;
     }
@@ -21,6 +25,24 @@ public class OneEditAway {
                     return false;
                 }
                 foundDifference = true;
+            }
+        }
+        return true;
+    }
+
+    public static boolean oneEditInsert(String s1, String s2) {
+        int index1 = 0;
+        int index2 = 0;
+
+        while (index1 < s1.length() && index2 < s2.length()) {
+            if (s1.charAt(index1) != s2.charAt(index2)) {
+                if (index1 != index2) {
+                    return false;
+                }
+                index2++;
+            } else {
+                index1++;
+                index2++;
             }
         }
         return true;
